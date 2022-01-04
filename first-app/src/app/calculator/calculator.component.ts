a7c1989f303f89d7be6bc6b1b75686d3bf6d2ab9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-04 16:41:34 +0530

    calculator - assignment - solution

--- first-app/src/app/calculator/calculator.component.ts
+++ first-app/src/app/calculator/calculator.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  n1 : number = 0;
+  n2 : number = 0;
+  operation : string = '';
+  result : number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setN1(val : string){
+    this.n1 = parseInt(val);
+  }
+
+  setN2(val : string){
+    this.n2 = parseInt(val);
+  }
+
+  onBtnCalculateClick(){
+    switch (this.operation) {
+      case 'add':
+        this.result = this.n1 + this.n2
+        break;
+      case 'subtract':
+        this.result = this.n1 - this.n2
+        break;
+      case 'multiply':
+        this.result = this.n1 * this.n2
+        break;
+      case 'divide':
+        this.result = this.n1 / this.n2
+        break;
+    
+      default:
+        break;
+    }
+  }
 }
