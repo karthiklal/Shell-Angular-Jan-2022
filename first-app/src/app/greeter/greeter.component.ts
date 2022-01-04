8e994a1c665426902664b1cb6d66b869b7ddf203
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-04 14:38:18 +0530

    responding to events

--- first-app/src/app/greeter/greeter.component.ts
+++ first-app/src/app/greeter/greeter.component.ts
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
     styleUrls : ['greeter.component.css']
 })
 export class GreeterComponent{
+    message : string = '[Default greet message]';
 
+    onBtnGreetClick(){
+        this.message = 'greet button clicked';
+    }
 }
