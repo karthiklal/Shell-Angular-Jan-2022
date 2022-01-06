04f3f79ba2117fc789595c3d6f19e6d3a3b68977
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-06 10:34:42 +0530

    Persisting the bugs in the localStorage - part - 1

--- bug-tracker-app/src/app/bugs/bugs.component.ts
+++ bug-tracker-app/src/app/bugs/bugs.component.ts
@@ -13,11 +13,20 @@ export class BugsComponent implements OnInit {
   sortAttr : string = '';
   sortDesc : boolean = false;
   
+  private storage : Storage = window.localStorage;
+
   private _currentBugId : number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    for(let index=0; index < this.storage.length; index++){
+      let key = this.storage.key(index) || '',
+        data = this.storage.getItem(key) || '',
+        bug = JSON.parse(data);
+        this.bugs.push(bug);
+        this._currentBugId = this._currentBugId > bug.id ? this._currentBugId : bug.id;
+    }
   }
 
   onBtnAddNewClick(newBugName : string){
@@ -27,19 +36,26 @@ export class BugsComponent implements OnInit {
       isClosed : false,
       createdAt : new Date()
     };
+    this.storage.setItem(newBug.id.toString(), JSON.stringify(newBug));
     this.bugs.push(newBug);
   }
 
   onBtnRemoveClick(bugToRemove : Bug){
+    this.storage.removeItem(bugToRemove.id.toString());
     this.bugs.splice(this.bugs.indexOf(bugToRemove), 1);
   }
 
   onBugNameClick(bugToToggle : Bug){
     bugToToggle.isClosed = !bugToToggle.isClosed;
+    this.storage.setItem(bugToToggle.id.toString(), JSON.stringify(bugToToggle));
   }
 
   onBtnRemoveClosedClick(){
-    this.bugs = this.bugs.filter(bug => !bug.isClosed);
+    const closedBugs = this.bugs.filter(bug => bug.isClosed);
+    closedBugs.forEach(bug => {
+      this.storage.removeItem(bug.id.toString())
+      this.bugs.splice(this.bugs.indexOf(bug), 1);
+    });
   }
 
   getClosedCount(): number {
